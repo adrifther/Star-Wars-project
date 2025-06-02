759333e247974f222bc9e9c4cecf7e4df0eae2b0
cheftonic92 <ab.saez.1992@gmail.com>
2025-06-02 12:24:43 +0200

    Commit por si acaso

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://www.swapi.tech/api'
-const URL_IMG = 'https://starwars-databank-server.vercel.app/api/v1/characters/name/';
+const PEOPLE_URL = 'https://starwars-databank-server.vercel.app/api/v1/characters/name/';
+const PLANETS_URL = 'https://starwars-databank-server.vercel.app/api/v1/planets/name/';
+const VEHICLES_URL = 'https://starwars-databank-server.vercel.app/api/v1/vehicles/name/';
 
 const swapiService = axios.create({
   baseURL: BASE_URL,
@@ -10,12 +12,27 @@ const swapiService = axios.create({
   },
 });
 
-const imgService = axios.create({
-  baseURL: URL_IMG,
+const peopleService = axios.create({
+  baseURL: PEOPLE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
+const planetsService = axios.create({
+  baseURL: PLANETS_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+const vehiclesService = axios.create({
+  baseURL: VEHICLES_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
 
 export const getAllPeople = async () => {
     try{
@@ -27,6 +44,7 @@ export const getAllPeople = async () => {
         throw error;
     }
 }
+
 export const getAllPlanets = async () => {
     try{
         const response = await swapiService.get('/planets?page=1&limit=60');
@@ -47,8 +65,71 @@ export const getAllVehicles = async () => {
         console.error("Error fetching vehicles data:", error);
         throw error;
     }
-}   
-// export const getAllPlanets = swapiService.get('/planets?page=1&limit=60');
-// export const getAllVehicles = swapiService.get('/vehicles?page=1&limit=60');
+}
 
-export const getPersonById = (id) => swapiService.get(`/people/${id}`);
+export const getPersonById = async (id) => {
+    try {
+        const response = await swapiService.get(`/people/${id}`);
+        return response.data.result.properties;
+    }
+    catch (error) {
+        console.error(`Error fetching person with ID ${id}:`, error);
+        throw error;
+    }
+}
+
+export const getPlanetById = async (id) => {
+    try {
+        const response = await swapiService.get(`/planets/${id}`);
+        return response.data.result.properties;
+    }
+    catch (error) {
+        console.error(`Error fetching planet with ID ${id}:`, error);
+        throw error;
+    }
+}
+
+export const getVehicleById = async (id) => {
+    try {
+        const response = await swapiService.get(`/vehicles/${id}`);
+        return response.data.result.properties;
+    }
+    catch (error) {
+        console.error(`Error fetching vehicle with ID ${id}:`, error);
+        throw error;
+    }
+}
+
+export const getPeopleDetailsByName = async (name) => {
+    try {
+        const response = await peopleService.get(name);
+        return response.data;
+    }
+    catch (error) {
+        console.error(`Error fetching details for ${name}:`, error);
+        throw error;
+    }
+}
+
+export const getPlanetsDetailsByName = async (name) => {
+    try {
+        const response = await planetsService.get(name);
+        return response.data;
+    }
+    catch (error) {
+        console.error(`Error fetching details for ${name}:`, error);
+        throw error;
+    }
+}
+
+export const getVehiclesDetailsByName = async (name) => {
+    try {
+        const response = await vehiclesService.get(name);
+        return response.data;
+    }
+    catch (error) {
+        console.error(`Error fetching details for ${name}:`, error);
+        throw error;
+    }
+}
+
